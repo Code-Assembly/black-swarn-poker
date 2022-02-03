97eb46376d574cdab816e08a2134018384434524
Aadil Cachalia <aadil.cachalia@camcog.com>
2022-02-03 03:46:41 +0200

    Add button style variant

--- src/game/components/Button.tsx
+++ src/game/components/Button.tsx
@@ -6,14 +6,15 @@ const StyledButton = styled.button<ButtonProps>`
 	padding: 0.5em 1.2em;
 	min-width: max-content;
 
+	font-size: 1em;
+	line-height: 1.2;
+	font-weight: 700;
 	color: #eee;
 
-	font-size: 1em;
-	line-height: 1;
-	font-weight: 700;
-
-	background-color: #89d4cf;
-	background-image: linear-gradient(315deg, #89d4cf 0%, #6e45e1 74%);
+	${(props) =>
+		props.color !== 'Red'
+			? 'background-color: #89d4cf; background-image: linear-gradient(315deg, #89d4cf 0%, #6e45e1 74%);'
+			: 'background-color: #a40606; background-image: linear-gradient(315deg, #a40606 0%, #d98324 74%)'};
 
 	border: 0px;
 	border-radius: 3px;
@@ -28,9 +29,10 @@ const StyledButton = styled.button<ButtonProps>`
 	}
 `;
 
-export type ButtonProps = {
+type ButtonProps = {
 	children?: HTMLCollection | string;
 	onClick: (e?: React.MouseEvent) => void;
+	color?: 'Red' | 'Blue';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = ({ onClick, children, ...otherProps }: ButtonProps) => {
